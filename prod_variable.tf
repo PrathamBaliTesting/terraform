b8fc7bee5b2d9fdcff429401f96f017f687070c8
pratham-baliyan-3057 <pratham.baliyan@octobit8.com>
2024-08-20 18:31:06 +0530

    modified:   prod_variable.tf 	deleted:    production.tfvars

--- prod_variable.tf
+++ prod_variable.tf
@@ -6,13 +6,16 @@ variable "vpc_region" {
 
 variable "vpc_cidr" {
   description = "VPC CIDR Block"
+  default = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidr" {
   description = "Public Subnet CIDR"
+  default = "10.0.1.0/16"
 }
 
 variable "private_subnet_cidr" {
   description = "Private Subnet CIDR"
+  default = "10.0.2.0/16"
 }
 
