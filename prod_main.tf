5ae6f2b1c43be81d02ff1fc9679fb1229c0accea
pratham-baliyan-3057 <pratham.baliyan@octobit8.com>
2024-08-20 18:10:07 +0530

    modified:   prod_main.tf

--- prod_main.tf
+++ prod_main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.63.0"
+    }
+    # other providers as needed
+  }
+}
 provider "aws" {
   region = var.vpc_region
 }
